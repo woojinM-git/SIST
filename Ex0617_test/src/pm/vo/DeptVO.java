100d7606e64f949a0a6ff060419fac4d93712c22
mwj02 <145192679+woojinM-git@users.noreply.github.com>
2025-06-17 17:53:22 +0900

    Add files via upload

---
+++ Ex0617_test/src/pm/vo/DeptVO.java
@@ -0,0 +1,41 @@
+package pm.vo;
+
+import java.util.List;
+
+public class DeptVO {
+    private String deptno, dname, loc_code;
+
+    private LocVO lvo;
+
+    public LocVO getLov() {
+        return lvo;
+    }
+
+    public void setLov(LocVO lov) {
+        this.lvo = lvo;
+    }
+
+    public String getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(String deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getLoc_code() {
+        return loc_code;
+    }
+
+    public void setLoc_code(String loc_code) {
+        this.loc_code = loc_code;
+    }
+}

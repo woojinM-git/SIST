a6aa2bfcb15b7b60ee6265ee88a37498d7601422
mwj02 <moonwj5685@gmail.com>
2025-10-08 14:27:28 +0900

    123
    123

---
+++ BackJoon_Study/work/calculation/10171.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ BackJoon_Study/work/calculation/10172.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+    }
+}
